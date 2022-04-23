897227d57c7678e0e79c705129390a4f8a103c41
ShikariBaaZ <91068760+ShikariBaaZ@users.noreply.github.com>
2022-04-23 19:03:40 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,78 @@
+'''MIT License
+
+Copyright (c) 2021 Shikari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
+
+
+import requests
+from telethon import TelegramClient
+from telethon import events
+import os
+import logging
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# Basics
+APP_ID = os.environ.get("APP_ID", default=None)
+API_HASH = os.environ.get("API_HASH", default=None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", default=None)
+
+
+bot = TelegramClient("News_Bot", APP_ID, API_HASH)
+start = bot.start(bot_token=BOT_TOKEN) 
+
+
+
+@bot.on(events.NewMessage(pattern="^/start"))
+async def _(event):
+    await event.reply('''Hey, i am your Newspaper bot , you can use me to read daily updated news. \njust hit command - \n /news \n /news bollywood \nit will send you all news in bot DM. \n \n Made by @The_Shikarii ''')
+
+
+
+@bot.on(events.NewMessage(pattern="^/news ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    infintyvar = event.pattern_match.group(1)
+    main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
+    stuber = await event.reply(
+        f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
+    )
+    await stuber.edit("All News Has Been Sucessfully fetched, sending to you.")
+    starknews = requests.get(main_url).json()
+    for item in starknews["data"]:
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        await bot.send_file(event.chat_id, sed1, caption=sedm)
+
+
+
+
+
+print ("Successfully Started")
+start.run_until_disconnected()
